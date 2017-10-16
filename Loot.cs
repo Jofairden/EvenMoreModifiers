9200d518d5b319cc60221790f9f9036bb679cb9b
Jofairden <danielzondervan.nl@gmail.com>
2017-10-16 20:27:17 +0200

    Formatting

--- Loot.cs
+++ Loot.cs
@@ -4,34 +4,34 @@ using Terraria.ModLoader;
 
 namespace Loot
 {
-    public class Loot : Mod
-    {
-	    internal static Loot Instance;
+	public class Loot : Mod
+	{
+		internal static Loot Instance;
 
-        public Loot()
-        {
-            Properties = new ModProperties()
-            {
-                Autoload = true,
-                AutoloadGores = true,
-                AutoloadSounds = true
-            };
-        }
+		public Loot()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
 
-	    public override void Load()
-	    {
-		    Instance = this;
-	    }
+		public override void Load()
+		{
+			Instance = this;
+		}
 
-	    public override void Unload()
-	    {
-		    Instance = null;
-	    }
+		public override void Unload()
+		{
+			Instance = null;
+		}
 
-	    // @todo: probably write our own handler for packets
-        public override void HandlePacket(BinaryReader reader, int whoAmI)
-        {
-            
-        }
-    }
+		// @todo: probably write our own handler for packets
+		public override void HandlePacket(BinaryReader reader, int whoAmI)
+		{
+
+		}
+	}
 }
