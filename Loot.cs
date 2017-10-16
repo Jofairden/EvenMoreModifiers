6d7766a03311b0103fa2677a6a7fa6d3f4d25b6d
Jofairden <danielzondervan.nl@gmail.com>
2017-10-16 20:02:36 +0200

    Clean base of operations

--- Loot.cs
+++ Loot.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using Terraria;
 using Terraria.ModLoader;
-using Loot.Items;
 
 namespace Loot
 {
@@ -19,29 +18,7 @@ namespace Loot
 
         public override void HandlePacket(BinaryReader reader, int whoAmI)
         {
-            string msgType = reader.ReadString();
-            if (msgType.Equals("Prefix"))
-            {
-                Item item = Main.item[reader.ReadInt32()];
-                gItem.addPrefix(item, reader.ReadInt32(), (float)reader.ReadDouble(), reader.ReadInt32());
-            }
-            else if (msgType.Equals("SyncRolled"))
-            {
-                Item item = Main.item[reader.ReadInt32()];
-                EMMItem info = item.GetGlobalItem<EMMItem>();
-                info.alreadyRolled = reader.ReadBoolean();
-                info.title = reader.ReadString();
-            }
-            else if (msgType.Equals("Reset"))
-            {
-                Item item = Main.item[reader.ReadInt32()];
-                EMMItem info = item.GetGlobalItem<EMMItem>();
-                info.alreadyRolled = false;
-                info.prefixIDs = new int[] { -1, -1, -1, -1 };
-                info.prefixMagnitude = new int[] { -1, -1, -1, -1 };
-                info.prefixMagnitudePercent = new double[] { -1, -1, -1, -1 };
-                info.title = "";
-            }
+            
         }
     }
 }
