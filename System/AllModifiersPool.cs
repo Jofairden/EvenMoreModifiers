4b5ac9d761c2ee0ffca27857006e867ecd22d20c
Jofairden <danielzondervan.nl@gmail.com>
2018-03-28 08:21:50 +0200

    Omit AllModifiersPool from the predefined pools

--- System/AllModifiersPool.cs
+++ System/AllModifiersPool.cs
@@ -4,6 +4,8 @@ namespace Loot.System
 {
 	public sealed class AllModifiersPool : ModifierPool
 	{
+		public override float RollChance => 0f;
+
 		public AllModifiersPool()
 		{
 			Modifiers = EMMLoader.Modifiers.Select(x => x.Value).ToArray();
