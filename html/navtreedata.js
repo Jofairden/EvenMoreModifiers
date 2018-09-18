1cfeb1dc714a47341875f2f2d704d9ac59db6a21
Travis CI <travis@travis-ci.org>
2018-09-18 21:08:22 +0000

    Deploy code docs to GitHub Pages Travis build: 1
    Commit: 9b7e9fe02f9b89dbe7c14711116eb3eb439e85d0

---
+++ html/navtreedata.js
@@ -0,0 +1,12 @@
+var NAVTREE =
+[
+  [ "Even More Modifiers", "index.html", ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
